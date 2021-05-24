f8c574bbfcecd89ba6f0e674936206323a37e45a
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-05-24 18:13:33 +0300

    added 8 async challenges

---
+++ callbackAsyncExersices.js
@@ -0,0 +1,66 @@
+/* CHALLENGE 2 */
+function delayedGreet() {
+  setTimeout(() => console.log('welcome'), 3000)
+}
+// delayedGreet(); // should log (after 3 seconds): welcome
+
+
+/* CHALLENGE 3 */
+function helloGoodbye() {
+  console.log('hello')
+  setTimeout(() => console.log('goodbye'), 2000)
+}
+// helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+
+
+/* CHALLENGE 4 */
+function brokenRecord() {
+  setInterval(()=> console.log('hi again'), 1000)
+}
+// brokenRecord(); // should log (every second): hi again
+
+
+/* CHALLENGE 5 */
+function limitedRepeat() {
+  const id = setInterval(()=> console.log('hi for now'), 1000)
+  setTimeout(() => clearInterval(id), 5000);
+}
+// limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+
+
+/* CHALLENGE 6 */
+function everyXsecsForYsecs(func, interval, duration) {
+  const id = setInterval(func, interval)
+  setTimeout(() => clearInterval(id), duration)
+}
+
+function theEnd() {
+  console.log('This is the end!');
+}
+// everyXsecsForYsecs(theEnd, 2000, 5000); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+
+/* CHALLENGE 7 */
+function delayCounter(target, wait) {
+  let counter = 0
+  function count() {
+    if (target === counter) return counter
+    setTimeout(() => console.log(count()), wait)
+    return counter++
+  }
+  
+  return count
+}
+
+const countLogger = delayCounter(3, 1000)
+// countLogger();
+
+
+/* CHALLENGE 8 */
+function promised() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000)
+	})
+}
+const createPromise = promised();
+// createPromise.then(() => console.log('wait for it...')); 
--- closureExersices.js
+++ closureExersices.js
@@ -132,6 +132,77 @@ function saveOutput(func, magicWord) {
 
 const multiplyBy2 = function(num) { return num * 2; };
 const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-console.log(multBy2AndLog(2)); // => should log 4
-console.log(multBy2AndLog(9)); // => should log 18
-console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+// console.log(multBy2AndLog(2)); // => should log 4
+// console.log(multBy2AndLog(9)); // => should log 18
+// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+
+// CHALLENGE 19
+function blackjack(array) {
+  let count = 0
+
+  function dealer(num1, num2) {
+    let sum = num1 + num2
+    let isBust = false
+    let isFirstTime = true
+    
+    function player() {
+      if (count === 0 || isFirstTime){
+        isFirstTime = false
+        count++
+        return sum
+      }
+       let result = sum + array[count - 1]
+       if (result <= 21) {
+          count++
+         	sum = result
+          return result
+        }
+      
+     	 if (!isBust) {
+        // count++
+        isBust = true
+        return 'bust'
+      } return 'you are done!' 
+      // sum = sum + array[count - 2] 
+    }
+    
+    return player
+  }
+  
+  return dealer
+}
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+const i_like_to_live_dangerously = deal(4, 5);
+// console.log(i_like_to_live_dangerously()); // => should log 9
+// console.log(i_like_to_live_dangerously()); // => should log 11
+// console.log(i_like_to_live_dangerously()); // => should log 17
+// console.log(i_like_to_live_dangerously()); // => should log 18
+// console.log(i_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+const i_TOO_like_to_live_dangerously = deal(2, 2);
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+const i_ALSO_like_to_live_dangerously = deal(3, 7);
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+
